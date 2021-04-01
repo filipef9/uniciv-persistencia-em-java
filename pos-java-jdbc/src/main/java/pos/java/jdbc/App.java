9823ea553adc8422932baf797ffa74f96ee3bcd9
filipef9 <filipef9@hotmail.com>
2021-04-01 21:18:56 +0000

    Initial commit

---
+++ pos-java-jdbc/src/main/java/pos/java/jdbc/App.java
@@ -0,0 +1,13 @@
+package pos.java.jdbc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
