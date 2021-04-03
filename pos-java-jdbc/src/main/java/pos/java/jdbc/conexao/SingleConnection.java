d10b06367caf334141ebdd83d35dc642fefbde9c
filipef9 <filipef9@hotmail.com>
2021-04-03 01:46:43 +0000

    Implementação da classe com as configurações e conexão com o banco de dados.

---
+++ pos-java-jdbc/src/main/java/pos/java/jdbc/conexao/SingleConnection.java
@@ -0,0 +1,39 @@
+package pos.java.jdbc.conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import static java.util.Objects.isNull;
+
+public class SingleConnection {
+
+    private static Connection connection = null;
+    private static String url = "jdbc:postgresql://localhost:5432/posjava";
+    private static String user = "gitpod";
+    private static String password = "";
+
+    static {
+        conectar();
+    }
+
+    public SingleConnection() {
+        conectar();
+    }
+
+    private static void conectar() {
+        try {
+            if (isNull(connection)) {
+                Class.forName("org.postgresql.Driver");
+                connection = DriverManager.getConnection(url, user, password);
+                connection.setAutoCommit(false);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
+
+}
