bd90c66455b1d537f44dba3dad814f546b44138a
filipef9 <filipef9@hotmail.com>
2021-04-04 05:28:26 +0000

    Aula PJ-UN01-09

--- pos-java-jdbc/src/main/java/pos/java/jdbc/dao/UserDao.java
+++ pos-java-jdbc/src/main/java/pos/java/jdbc/dao/UserDao.java
@@ -2,6 +2,10 @@ package pos.java.jdbc.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import pos.java.jdbc.model.User;
 
@@ -17,7 +21,7 @@ public class UserDao {
         try {
             final String sql = "INSERT INTO tbl_user (id, nome, email) VALUES (?, ?, ?)";
             PreparedStatement insert = connection.prepareStatement(sql);
-            insert.setLong(1, user.getId());
+            insert.setLong(1, getNextId());
             insert.setString(2, user.getNome());
             insert.setString(3, user.getEmail());
             insert.execute();
@@ -31,4 +35,55 @@ public class UserDao {
             e.printStackTrace();
         }
     }
+
+    private Long getNextId() {
+        final List<User> users = listar();
+        users.sort((u1, u2) -> u1.getId().compareTo(u2.getId()));
+        return users.get(users.size() - 1).getId() + 1;
+    }
+
+    public List<User> listar() {
+        try {
+            List<User> usuarios = new ArrayList<>();
+            final String sql = "SELECT * FROM tbl_user";
+            final PreparedStatement select = connection.prepareStatement(sql);
+            final ResultSet resultado = select.executeQuery();
+            while (resultado.next()) {
+                User user = User.of(
+                    resultado.getLong("id"),
+                    resultado.getString("nome"),
+                    resultado.getString("email")
+                );
+                usuarios.add(user);
+            }
+
+            return usuarios;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Collections.emptyList();
+        }
+    }
+
+	public User buscarPorId(long id) {
+		try {
+           final String sql = "SELECT * FROM tbl_user WHERE id = " + id;
+           final PreparedStatement select = connection.prepareStatement(sql);
+           final ResultSet resultado = select.executeQuery();
+
+           User userFound = null;
+           while (resultado.next()) {
+                userFound = User.of(
+                    resultado.getLong("id"),
+                    resultado.getString("nome"),
+                    resultado.getString("email")
+                );
+           }
+
+           return userFound;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+	}
+
 }
--- pos-java-jdbc/src/test/java/pos/java/jdbc/dao/UserDaoTest.java
+++ pos-java-jdbc/src/test/java/pos/java/jdbc/dao/UserDaoTest.java
@@ -1,11 +1,17 @@
 package pos.java.jdbc.dao;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import java.sql.Connection;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -25,17 +31,44 @@ public class UserDaoTest {
         connection = SingleConnection.getConnection();
         connectionSpy = spy(connection);
         userDao = new UserDao(connectionSpy);
-        user = User.of(1005L, "John Doe", "john.doe@gmail.com");
+        user = User.of(null, "John Doe", "john.doe@gmail.com");
 
         // act:
         userDao.salvar(user);
 
         // assert:
         try {
-            verify(connectionSpy, times(1)).prepareStatement(anyString());
+            verify(connectionSpy, times(2)).prepareStatement(anyString());
             verify(connectionSpy, times(1)).commit();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    @Test
+    public void listar_usuarios() {
+        // arrange:
+        connection = SingleConnection.getConnection();
+        userDao = new UserDao(connection);
+
+        // act:
+        List<User> usuarios = userDao.listar();
+
+        // assert:
+       assertThat(usuarios, is(not(empty())));
+    }
+
+    @Test
+    public void buscar_por_id() {
+        // arrange:
+        connection = SingleConnection.getConnection();
+        userDao = new UserDao(connection);
+
+        // act:
+        final User userFound = userDao.buscarPorId(1L);
+
+        // assert:
+        assertThat(userFound, is(notNullValue()));
+    }
+
 }
