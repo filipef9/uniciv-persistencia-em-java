b203da7dff9d4fe970b3f765b605b49b8046ded7
filipef9 <filipef9@hotmail.com>
2021-10-02 05:01:43 +0000

    Atualização das configurações do persistence.xml

---
+++ postgres/docker-entrypoint-initdb.d/10-create-database-posjavahibernate.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE posjavahibernate
+    WITH OWNER = admin
+        ENCODING = 'UTF8'
+        TABLESPACE = pg_default
+        LC_COLLATE = 'Portuguese_Brazil.1252'
+        LC_TYPE = 'Portuguese_Brazil.1252'
+        CONNECTION LIMIT = -1;
