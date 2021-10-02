6fd6a604a98768ceb1d9aecbf34199740854f418
filipef9 <filipef9@hotmail.com>
2021-10-02 05:49:34 +0000

    Implementação do HibernateUtil.

---
+++ pos-java-maven-hibernate/src/test/java/br/edu/uniciv/utils/HibernateUtilTest.java
@@ -0,0 +1,11 @@
+package br.edu.uniciv.utils;
+
+import org.junit.Test;
+
+public class HibernateUtilTest {
+    
+    @Test
+    public void testeHibernateUtil() {
+        HibernateUtil.getEntityManager();
+    }
+}
