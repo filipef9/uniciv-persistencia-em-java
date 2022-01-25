8dd19711d214342c75dfda9e507b66e2e7edaeee
filipef9 <filipef9@hotmail.com>
2022-01-25 21:14:50 +0000

    Persistência em Java - Unidade 3 - Aula 8
    Spring Data - Realizando Cadastros no Banco de Dados

--- projeto-spring-data/src/test/java/br/edu/uniciv/AppSpringDataTest.java
+++ projeto-spring-data/src/test/java/br/edu/uniciv/AppSpringDataTest.java
@@ -1,6 +1,9 @@
 package br.edu.uniciv;
 
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +11,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import br.edu.uniciv.dao.InterfaceSpringDataUser;
+import br.edu.uniciv.model.UsuarioSpringData;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:META-INF/spring-config.xml" })
@@ -17,8 +21,20 @@ public class AppSpringDataTest {
     private InterfaceSpringDataUser dao;
     
     @Test
-    public void iniciouSprintTesteComSucesso() {
-        System.out.println("Iniciou Spring com sucesso!");
+    public void testeInsert() {
+        // Arrange
+        final UsuarioSpringData aNewUser = new UsuarioSpringData();
+        aNewUser.setEmail("javaavancacdo@javaavancado.com");
+        aNewUser.setIdade(38);
+        aNewUser.setLogin("filipe.fsn");
+        aNewUser.setSenha("12345678");
+        aNewUser.setNome("Filipe");
+
+        // Act
+        final UsuarioSpringData savedUser = dao.save(aNewUser);
+
+        // Assert
+        assertThat(savedUser, notNullValue());
     }
 
 }
